cfdb0757b941e7c1ed00de85805cb936fc743f97
Alekseeva AA <glance93@yandex.ru>
2016-12-02 11:53:02 +0300

    Выполнение задания 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System; //используем пространство имен System
+
+namespace Peremennyie
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int p1 = 50; // 1 значимая целочисленная переменная
+
+            char p2;//2 значимая символьная переменная
+            p2 = '9';
+
+            object p3=p1; //3 переменная ссылочного типа
+
+            double p4 = 11 / 5;//4 значимая переменная с плавающей точкей
+
+            bool p5 = default(bool); //5 переменная значимого типа
+
+
+            
+
+        }
+    }
+}
